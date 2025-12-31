34eac990346a4242e62f37e6d7258eda18c06f60
Ali <ahmadi3d.ali.wcftdas@gmail.com>
2025-12-31 17:29:57 +0330

    improved ai module

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,25 +1,26 @@
 export * from "./core/protocol/errors";
-export * from "./ai/protocol/ids";
 
 import { ATAPIErrorCodeMap } from "./core/protocol/errors";
+
 import { ATAIID } from "./ai/protocol/ids";
 import { ATAIPrompts } from "./ai/protocol/prompts";
 import { ATAICapabilities } from "./ai/protocol/capabilities";
 
-export const ATProtocol = {
-    ATAPIErrorCodeMap,
-    ATID: {
-        ai: ATAIID,
-    },
-    ATAIPrompts,
-    ATAICapabilities
+export const ATAI = {
+    ID: ATAIID,
+    Prompts: ATAIPrompts,
+    Capabilities: ATAICapabilities,
 } as const;
 
-export namespace ATProtocol {
-    export type ATAPIErrorCode =
-        import("./core/protocol/errors").ATAPIErrorCode;
-    export type ATAICapabilityID =
-        import("./ai/protocol/ids").ATAICapabilityID;
-    export type ATAIPromptID =
-        import("./ai/protocol/ids").ATAIPromptID;
-}
+export const ATProtocol = {
+    ATAPIErrorCodeMap,
+    AI: ATAI,
+} as const;
+
+export type ATAICapabilityID =
+    import("./ai/protocol/ids").ATAICapabilityID;
+export type ATAIPromptID =
+    import("./ai/protocol/ids").ATAIPromptID;
+
+export type ATAPIErrorCode =
+    import("./core/protocol/errors").ATAPIErrorCode;
