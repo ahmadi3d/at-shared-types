9ca88373566cd49a71013698be252b8778044200
Ali <ahmadi3d.ali.wcftdas@gmail.com>
2025-12-31 16:48:21 +0330

    added ATID

--- src/protocol.ts
+++ src/protocol.ts
@@ -1,10 +1,21 @@
 export * from "./core/protocol/errors";
+export * from "./ai/protocol/ids";
 
 import { ATAPIErrorCodeMap } from "./core/protocol/errors";
+import { ATAIID } from "./ai/protocol/ids";
 
-export const ATProtocol = { ATAPIErrorCodeMap } as const;
+export const ATProtocol = {
+    ATAPIErrorCodeMap,
+    ATID: {
+        ai: ATAIID,
+    }
+} as const;
 
 export namespace ATProtocol {
     export type ATAPIErrorCode =
         import("./core/protocol/errors").ATAPIErrorCode;
+    export type ATAICapabilityID =
+        import("./ai/protocol/ids").ATAICapabilityID;
+    export type ATAIPromptID =
+        import("./ai/protocol/ids").ATAIPromptID;
 }
